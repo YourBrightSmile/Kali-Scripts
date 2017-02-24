db58bfe29d8d594baaafc36dc6e8db73c764eb78
YourBrightSmile <914552504@qq.com>
2017-02-24 21:15:16 +0800

    scapy create

--- arp/arping/arping_ether.sh
+++ arp/arping/arping_ether.sh
@@ -7,14 +7,14 @@ fi
 interface=$1
 prefix=$(ifconfig $interface|grep 'inet' |head -n 1|cut -d "t" -f2|cut -d" " -f 2|cut -d "." -f1-3)
 if [ "$#" -eq 1 ];then
-	for((i=1;i<254;i++));do
+	for((i=1;i<=254;i++));do
 		echo -n -e "\r[$i/254] "
 		arping -c 1 $prefix.$i | grep "bytes from"|cut -d " " -f4-9 
 	done
 fi
 
 if [ "$#" -eq 2 ];then
-	for((i=1;i<254;i++));do
+	for((i=1;i<=254;i++));do
 		echo -n -e "\r[$i/254]"
 		arping -c 1 $prefix.$i | grep "bytes from"|cut -d " " -f4-9 >> $2
 		#arping -c 1 $prefix.$i | grep "bytes from"|cut -d " " -f4-9 >> $2 &
